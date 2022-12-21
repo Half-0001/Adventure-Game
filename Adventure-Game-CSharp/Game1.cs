848c580d173f43d1ade7fcffcfa3fb0bf42cb8bc
Half-0001 <nyleholdsworth@icloud.com>
2022-12-21 21:35:20 +0000

    c# project, map, sprite animation

---
+++ Adventure-Game-CSharp/Game1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Comora;
+using MonoGame.Aseprite.Documents;
+using MonoGame.Aseprite.Graphics;
+
+namespace Adventure_Game_CSharp
+{
+    enum Dir
+    {
+        Down,
+        Up,
+        Left,
+        Right,
+    }
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Point _resolution;
+
+        private AnimatedSprite _sprite;
+
+        //classes
+        Player player = new Player();
+        private Camera camera;
+
+        //textures
+        Texture2D background;
+
+        //variables
+        const int playerRadius = 20;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _resolution = new Point(900, 900);
+            _graphics.PreferredBackBufferWidth = _resolution.X;
+            _graphics.PreferredBackBufferHeight = _resolution.Y;
+            _graphics.ApplyChanges();
+            this.camera = new Camera(_graphics.GraphicsDevice);
+            camera.Zoom = 2f;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            background = Content.Load<Texture2D>("map");
+
+            //  Load the asprite file from the content pipeline.
+            AsepriteDocument aseprite = Content.Load<AsepriteDocument>("adventurer");
+
+            //  Create a new aniamted sprite instance using the aseprite doucment loaded.
+            _sprite = new AnimatedSprite(aseprite);
+            _sprite.Scale = new Vector2(1.0f, 1.0f);
+            _sprite.Y = _resolution.Y - (_sprite.Height * _sprite.Scale.Y) - 16;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            _sprite.Play("idle");
+
+            player.PlayerUpdate(gameTime);
+
+            this.camera.Position = player.Position;
+            this.camera.Update(gameTime);
+
+            _sprite.Update(dt);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin(this.camera);
+            _spriteBatch.Draw(background, new Vector2(0, 0), Color.White);
+
+            //draw player
+            //_spriteBatch.Draw(playerTexture, new Vector2(player.Position.X - playerRadius, player.Position.Y - playerRadius), Color.White);
+            _sprite.Render(_spriteBatch);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Adventure-Game-CSharp/Player.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Adventure_Game_CSharp
+{
+    internal class Player
+    {
+        private Vector2 position = new Vector2(500, 500);
+        private int speed = 300;
+        private Dir direction = Dir.Down;
+        private bool isMoving = false;
+        private KeyboardState kStateOld = Keyboard.GetState();
+        
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+        public void PlayerUpdate(GameTime gameTime)
+        {
+            KeyboardState kState = Keyboard.GetState();
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            isMoving = false;
+
+            if (kState.IsKeyDown(Keys.D))
+            {
+                direction = Dir.Right;
+                isMoving = true;
+            }
+
+            if (kState.IsKeyDown(Keys.A))
+            {
+                direction = Dir.Left;
+                isMoving = true;
+            }
+
+            if (kState.IsKeyDown(Keys.W))
+            {
+                direction = Dir.Up;
+                isMoving = true;
+            }
+
+            if (kState.IsKeyDown(Keys.S))
+            {
+                direction = Dir.Down;
+                isMoving = true;
+            }
+            if (kState.IsKeyDown(Keys.Space))
+                isMoving = false;
+
+            if (isMoving)
+            {
+
+                switch (direction)
+                {
+                    case Dir.Left:
+                        position.X -= speed * dt;
+                        break;
+                    case Dir.Up:
+                        position.Y -= speed * dt;
+                        break;
+                    case Dir.Right:
+                        position.X += speed * dt;
+                        break;
+                    case Dir.Down:
+                        position.Y += speed * dt;
+                        break;
+                }
+            }
+        }
+
+        public void PlayerDraw(SpriteBatch _spriteBatch)
+        {
+
+        }
+    }
+}
